71246a344f23464795a414f3db6fb888f1e654ea
wntdev99 <154935538+wntdev99@users.noreply.github.com>
2024-02-28 02:44:54 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,7 @@
+inputNumberFirst = input()
+inputNumberSecond = input()
+
+print(int(inputNumberFirst) * int(inputNumberSecond[2]))
+print(int(inputNumberFirst) * int(inputNumberSecond[1]))
+print(int(inputNumberFirst) * int(inputNumberSecond[0]))
+print(int(inputNumberFirst) * int(inputNumberSecond))
