776ea6561da390e2cb2d259b73ab23aa63727292
wntdev99 <154935538+wntdev99@users.noreply.github.com>
2024-02-28 19:37:28 +0900

    [Bronze I] Title: 평균, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,5 +1,4 @@
 import sys
 inputString = sys.stdin.readlines()
 inputNumbers = list(map(int, inputString[1].split()))
-setNumbers = list(map(lambda x: x / max(inputNumbers) * 100, inputNumbers))
-print(sum(setNumbers) / int(inputString[0]))
+print(sum(list(map(lambda x: x / max(inputNumbers) * 100 / int(inputString[0]), inputNumbers))))
