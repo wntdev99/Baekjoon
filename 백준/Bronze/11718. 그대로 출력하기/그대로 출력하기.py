c6358cbc770f0a657a3b6667304b2d677a987ffd
wntdev99 <154935538+wntdev99@users.noreply.github.com>
2024-02-28 14:43:51 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -1,11 +1,2 @@
-lines = []
-for _ in range(100):
-    try:
-        line = input()
-        if line:
-            lines.append(line)
-    except EOFError:
-        break
-
-for line in lines:
-    print(line)
+import sys
+print(sys.stdin.read())
