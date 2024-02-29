fbce6ea55cef70ad7bbab07db1c69b2a797f4ab9
wntdev99 <154935538+wntdev99@users.noreply.github.com>
2024-02-29 19:07:07 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,16 +1,14 @@
 import sys
 
-totalClass = 0.0
+GRADEDICTIONARY = {"A+":4.5, "A0":4.0, "B+":3.5, "B0":3.0, "C+":2.5, "C0":2.0, "D+":1.5, "D0":1.0, "F":0.0}
+
+totalRate = 0.0
 totalGrade = 0.0
 
 readLines = sys.stdin.readlines()
-for readLine in readLines:
-    className, classRate, classGrade = readLine.split()
-    if classGrade == 'P': 
-        continue
-    elif classGrade == 'F':
-        totalClass += float(classRate)
-    else:
-        totalClass += float(classRate)
-        totalGrade += (69.0 - ord(classGrade[0]) + (48.0 - ord(classGrade[1])) / 10.0) * float(classRate)
-print(totalGrade / totalClass)
+for readline in readLines:
+    className, classRate, classGrade = readline.split()
+    if classGrade == 'P': continue
+    totalRate += float(classRate)
+    totalGrade += GRADEDICTIONARY[classGrade] * float(classRate)
+print(totalGrade/totalRate)
